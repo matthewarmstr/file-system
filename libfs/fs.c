e6f6ffc7fc2e8e1226575a67f8989f9867c9137d
Matthew Armstrong <mgarmstrong@ucdavis.edu>
2023-05-23 18:53:09 -0700

    kept working on phase 1

--- libfs/fs.c
+++ libfs/fs.c
@@ -9,9 +9,10 @@
 
 #define SB_PADDING_LEN 4079
 #define SB_EXPECTED_SIG 6000536558536704837
-#define RD_PADDING_LEN 80
+#define FB_ENTRIES_PER_BLOCK 2048
+#define RD_PADDING_LEN 10
+#define FAT_EOC 0xFFFF
 
-/* TODO: Phase 1 */
 struct superblock {
 	int64_t signature;
 	int16_t num_blocks_on_disk;
@@ -22,14 +23,30 @@ struct superblock {
 	int8_t padding[SB_PADDING_LEN];
 };
 
-struct root_dir {
-	int128_t filename;
+struct FAT_block {
+	int16_t entries[FB_ENTRIES_PER_BLOCK];
+};
+
+struct FAT_node {
+	struct FAT_block* data;
+	struct FAT_node* next;
+};
+
+struct FAT_section {
+	struct FAT_node* start;
+	struct FAT_node* end;
+};
+
+struct root_directory {
+	int8_t filename[FS_FILENAME_LEN];
 	int64_t file_size;
 	int16_t first_data_block_index;
-	int8_t padding[RD_PADDING_LEN]
-}
+	int8_t padding[RD_PADDING_LEN];
+};
 
 struct superblock *superblk;
+struct root_directory *root_dir;
+struct FAT_section *FAT_nodes;
 
 int fs_mount(const char *diskname)
 {
@@ -39,7 +56,11 @@ int fs_mount(const char *diskname)
 
 	// Store superblock info
 	void * buf;
-	block_read(0, buf);
+	int readret = block_read(0, buf);
+	if (readret == -1) {
+		fprintf(stderr, "Could not read from block (superblock)\n");
+		return -1;
+	}
 	superblk = buf;
 
 	// Check that signature is correct
@@ -51,6 +72,31 @@ int fs_mount(const char *diskname)
 	int blkcount = block_disk_count();
 	if (blkcount != superblk->num_blocks_on_disk) {
 		return -1;
+	}
+
+	// Load FAT blocks
+	for (int8_t i = 1; i <= superblk->num_blocks_FAT; i++) {		
+		struct FAT_block* new_FAT_blk = (struct FAT_block*)malloc(sizeof(struct FAT_block));
+		struct FAT_node* new_FAT_node = (struct FAT_node*)malloc(sizeof(struct FAT_node));
+		if (new_FAT_blk == NULL || new_FAT_node == NULL) {
+			fprintf(stderr, "Malloc failed");
+			return -1;
+		}
+		new_FAT_node->data = new_FAT_blk;
+		if (i == 1) {
+			FAT_nodes->start = new_FAT_node;
+			FAT_nodes->end = new_FAT_node;
+		} else {
+			// struct FAT_node* prev_FAT_node = FAT_nodes->end;
+			FAT_nodes->end->next = new_FAT_node;
+			FAT_nodes->end = new_FAT_node;
+		}
+		block_read(i, buf);
+		FAT_nodes->end->data = buf;
+
+		if (i == superblk->num_blocks_FAT) {
+			FAT_nodes->end->next = NULL;
+		}
 	}
 
 	// Store root directory info
@@ -69,7 +115,6 @@ int fs_umount(void)
 
 	for (int i = 0; i < superblk->num_data_blocks; i++) {
 		void * buf_out;
-
 	}
 }
 
